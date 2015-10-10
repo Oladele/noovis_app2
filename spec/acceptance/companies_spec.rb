6396b09cd9ba3b19a673f0b07294846df6c43226
Oladele <oladele@gmail.com>
2015-10-10 04:28:28 -0400

    Tweaked API acceptance test/docs format & content

--- spec/acceptance/companies_spec.rb
+++ spec/acceptance/companies_spec.rb
@@ -1,6 +1,8 @@
 require "acceptance_helper"
 
 RSpec.resource "Companies" do
+  header "Content-Type", "application/vnd.api+json"
+
   get "/companies" do
     example "Listing companies" do
       do_request
--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -5,4 +5,15 @@ require 'rspec_api_documentation/dsl'
 RspecApiDocumentation.configure do |config|
   config.format = [:json, :combined_text, :html]
   config.api_name = "NNOA 2.0"
+
+  # copied from json-api-example 
+  # https://github.com/barelyknown/json-api-example
+  # limits "Headers" and "Response" details to key values (much cleaner)
+  config.request_headers_to_include = ["Content-Type", "Authorization"]
+  config.response_headers_to_include = ["Content-Type"]
+  config.curl_headers_to_filter = %w[Host Cookie]
+  config.post_body_formatter = proc do |params|
+    { data: params }.to_json if params.present?
+  end
+
 end
