2452d3380668bd7ac0e5079835218142ea3b2be7
Oladele <oladele@gmail.com>
2015-10-12 07:35:38 -0400

    format model specs into describe block groups

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  it { is_expected.to have_attribute :name }
+	describe "attributes" do
+  	it { is_expected.to have_attribute :name }
+	end
   
-  it "validates presence of name" do
-    subject.name = nil
-    subject.valid?
-    expect(subject.errors[:name]).to include "can't be blank"
-  end
+  describe "validations" do
+	  it "validates presence of name" do
+	    subject.name = nil
+	    subject.valid?
+	    expect(subject.errors[:name]).to include "can't be blank"
+	  end
+	end
 
 end
