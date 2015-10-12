4d723737c778611432016ad39790fd65745abbef
Oladele <oladele@gmail.com>
2015-10-12 06:56:15 -0400

    post /companines acceptance test initial pass

--- spec/acceptance/companies_spec.rb
+++ spec/acceptance/companies_spec.rb
@@ -3,14 +3,30 @@ require "acceptance_helper"
 RSpec.resource "Companies" do
   header "Content-Type", "application/vnd.api+json"
 
-  get "/companies" do
+  post "/companies" do
+  	parameter :type, <<-DESC, required: true
+      Should always be set to <code>companies</code>.
+    DESC
+    parameter :name, 
+    	"Company Name", 
+    	required: true, scope: :attributes
+
+    let(:type){ "companies"}
+    let(:name){ "ACME Company"}
     
+    example "Create a company" do
+    	do_request
+    	expect(status).to eq 201
+    end
+  end
+
+  get "/companies" do    
     example "Listing companies" do
     	FactoryGirl.create :company, name: "1st company"
     	FactoryGirl.create :company, name: "2nd company"
       do_request
-
       expect(status).to eq 200
     end
   end
+
 end
