1c87794ec51ac0183fa61d0b5de88b8a650f073a
Oladele <oladele@gmail.com>
2015-10-15 14:10:33 -0400

    network_site model validations

--- app/models/network_site.rb
+++ app/models/network_site.rb
@@ -1,3 +1,8 @@
 class NetworkSite < ActiveRecord::Base
   belongs_to :company
+
+  validates :name, presence: true
+  validates :company_id, presence: true
+  validates :name, uniqueness: { scope: [:company_id] }
+
 end
--- spec/models/network_site_spec.rb
+++ spec/models/network_site_spec.rb
@@ -11,10 +11,18 @@ RSpec.describe NetworkSite, type: :model do
 	    subject.valid?
 	    expect(subject.errors[:company_id]).to include "can't be blank"
 	  end
+	  
 	  it "validates presence of name" do
 	    subject.name = nil
 	    subject.valid?
 	    expect(subject.errors[:name]).to include "can't be blank"
+	  end
+
+	  it "validates the uniqueness of name scoped to company_id" do
+	    original = FactoryGirl.create(:network_site)
+	    duplicate = FactoryGirl.build(:network_site, company: original.company, name: original.name)
+	    duplicate.valid?
+	    expect(duplicate.errors[:name]).to include "has already been taken"
 	  end
 	end
 end
