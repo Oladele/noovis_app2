7160c9eaa6ed55f72c815d6780698db3c21633cd
Brandon Braun <brandon.braun@gmail.com>
2016-02-13 17:47:06 -0500

    add cable run routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :cable_runs
+  resource :import_cable_run
+  jsonapi_resources :cable_runs
   jsonapi_resources :companies
   jsonapi_resources :network_sites
   jsonapi_resources :buildings
