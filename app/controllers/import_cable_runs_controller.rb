45fd0fc666c9944c5e036e5c477a2aac778c06a2
Brandon Braun <brandon.braun@gmail.com>
2016-02-21 20:50:52 -0500

    create responses for import cable runs

--- app/controllers/import_cable_runs_controller.rb
+++ app/controllers/import_cable_runs_controller.rb
@@ -1,13 +1,11 @@
 class ImportCableRunsController < ActionController::Base
   def create
-    importer = ImportCableRun.new(
+    @importer = ImportCableRun.new(
       workbookFile: params[:workbookFile],
       sheet: params[:sheet],
       building_id: params[:building_id]
     )
 
-    respond_to do |format|
-      format.json { render json: { "success": "#{importer.save}" } }
-    end
+    render status: @importer.status, json: { message: @importer.message }
   end
 end
