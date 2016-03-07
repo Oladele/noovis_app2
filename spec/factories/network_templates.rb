3ec2fc57357210053486df8602854dfd23f40297
Brandon Braun <brandon.braun@gmail.com>
2016-03-06 19:35:31 -0500

    add specs for network template

--- spec/factories/network_templates.rb
+++ spec/factories/network_templates.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
   factory :network_template do
     name "MyString"
-description "MyString"
-hierarchy "MyString"
+    description "MyString"
+    hierarchy %w(olt_chassis pon_card building fdh splitter rdt room ont_sn ont_ge_mac)
   end
-
 end
--- spec/models/network_template_spec.rb
+++ spec/models/network_template_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe NetworkTemplate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+  	it { is_expected.to have_attribute :name }
+  	it { is_expected.to have_attribute :description }
+  	it { is_expected.to have_attribute :hierarchy }
+  end
+
+  describe "validations" do
+	  it "validates presence of name" do
+	    subject.name = nil
+	    subject.valid?
+	    expect(subject.errors[:name]).to include "can't be blank"
+	  end
+  end
 end
