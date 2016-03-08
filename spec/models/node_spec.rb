2f465d169be309f79fa09168b7113962adadc0b5
Brandon Braun <brandon.braun@gmail.com>
2016-03-07 21:07:02 -0500

    add node model spec

--- spec/models/node_spec.rb
+++ spec/models/node_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Node, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+  	it { is_expected.to have_attribute :network_graph_id }
+  	it { is_expected.to have_attribute :node_type_id }
+  	it { is_expected.to have_attribute :node_value }
+  	it { is_expected.to have_attribute :node_level }
+  	it { is_expected.to have_attribute :x_pos }
+  	it { is_expected.to have_attribute :y_pos }
+	end
+
+	describe "associations" do
+	  it "should belong to a network graph" do
+      network_graph = FactoryGirl.create(:network_graph)
+      node = network_graph.nodes.build()
+      expect(node.network_graph).to eq network_graph
+	  end
+	  
+	  it "should belong to a node type" do
+      node_type = FactoryGirl.create(:node_type)
+      node = node_type.nodes.build()
+      expect(node.node_type).to eq node_type
+	  end
+	end
+  
+  describe "validations" do
+  	it "validates presence of network_graph_id" do
+	    subject.network_graph_id = nil
+	    subject.valid?
+	    expect(subject.errors[:network_graph_id]).to include "can't be blank"
+	  end
+	  
+	  it "validates presence of node_type_id" do
+	    subject.node_type_id = nil
+	    subject.valid?
+	    expect(subject.errors[:node_type_id]).to include "can't be blank"
+	  end
+  end
 end
