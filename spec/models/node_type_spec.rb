4cbf8c54e879b194c6bb3732aea1dba64071d7f8
Brandon Braun <brandon.braun@gmail.com>
2016-03-07 19:40:04 -0500

    add model spec for node types

--- spec/models/node_type_spec.rb
+++ spec/models/node_type_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe NodeType, type: :model do
+  describe "attributes" do
+  	it { is_expected.to have_attribute :name }
+  	it { is_expected.to have_attribute :picture }
+  end
+
+  describe "validations" do
+	  
+	  it "validates presence of name" do
+	    subject.name = nil
+	    subject.valid?
+	    expect(subject.errors[:name]).to include "can't be blank"
+	  end
+
+	  it "validates the uniqueness of name" do
+	    original = FactoryGirl.create(:node_type)
+	    duplicate = FactoryGirl.build(:node_type, name: original.name)
+	    duplicate.valid?
+	    expect(duplicate.errors[:name]).to include "has already been taken"
+	  end
+	end
 end
