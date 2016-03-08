05fa31e88e3961b23a5fa0d346c2780e3c0af36f
Brandon Braun <brandon.braun@gmail.com>
2016-03-07 21:07:47 -0500

    add node model validations and references

--- app/models/network_graph.rb
+++ app/models/network_graph.rb
@@ -1,6 +1,7 @@
 class NetworkGraph < ActiveRecord::Base
   belongs_to :sheet
   belongs_to :network_template
+  has_many :nodes
 
   validates :sheet_id, presence: true
   validates :network_template_id, presence: true
