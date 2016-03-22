1777f5dfd695ba4394747fac3573d0a4284cffbf
Oladele <oladele@gmail.com>
2016-03-22 18:16:25 -0400

    Node Stats Class with node_counts methodw

---
+++ app/models/node_stats.rb
@@ -0,0 +1,35 @@
+class	NodeStats
+	def initialize(nodes)
+		@nodes = nodes
+	end
+
+	def node_counts(node_types)
+
+		# hash to store/lookup counts
+		node_type_counts = {}
+		
+		# set hash keys(node_type) and values(count) to zero
+		node_types.each do |node_type|
+			node_type_counts[node_type] = 0
+		end
+
+		# count each node by node_type
+		@nodes.each do |node|
+			node_types.each do |node_type|
+				if node.node_type == node_type
+					node_type_counts[node_type] +=1					
+				end
+			end
+		end
+
+		# return counts
+		node_counts = node_types.map do |node_type|
+			{
+				node_type: node_type,
+				node_type_pretty: node_type.capitalize,
+				count: node_type_counts[node_type]
+			}
+		end
+		
+	end
+end
