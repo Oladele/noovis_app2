bd7487a1901ce8d550653eaecf914ca2c4681186
Brandon Braun <brandon.braun@gmail.com>
2016-04-13 18:16:10 -0400

    adjust default for devise token auth

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "email#{n}@example.com" }
     company
+    password "password"
+    password_confirmation "password"
   end
 
 end
