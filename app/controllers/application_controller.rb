fbebbc05b7b13edf1c68194496bc4ca3677b9ff6
Brandon Braun <brandon.braun@gmail.com>
2016-05-12 20:32:59 -0400

    fix auth for import cable runs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base #JSONAPI::ResourceControlle
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  private
+
+  def admin_only
+    unless current_user.admin?
+      render status: :forbidden, json: { message: 'Access denied' }
+    end
+  end
 end
