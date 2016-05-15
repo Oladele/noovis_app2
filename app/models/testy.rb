cf6f0a3c3bc2e660693b34884df6d2e39da4a22d
Ola Adedoyin <oladele@gmail.com>
2016-05-14 21:19:25 -0400

    Update testy.rb
    So, I said I'm still figuring the PR collab process. Not sure if I'm actually supposed to somehow pull the code and run it but don't know how. Maybe if I did I'd see it all working. But stepping through it and commenting what I think the statements evaluate to, some things seem off to me. Did you run somehow run the code and the spec and can you somehow show me the inputs/outputs? Of course my manual checking likely has its own issues!!!! My most critical comments are:
    63 thru 68
    101,
    116 thru 119
    
    ...hope this is more helpful, than distracting. Please advise me on the best way to review/test/trial the changes

--- app/models/testy.rb
+++ app/models/testy.rb
@@ -57,23 +57,66 @@ class Testy
 
     values.each do |row|
       previous = graph  # Start at the top
+      # OLA for first row previous = graph = { sites: [] }
 
       row.each_with_index do |col, index|
+        # OLA: don't think we can assume that the row's column's index matches the template's index
+        # Illustrative eg: First iteration of this loop. Let's say first column in row is "olt"  for whatever reason 
+        # (remember order of sheet columns DON'T match template/network hierachy)
+        # then for the first iteration in this loop, below would evaluate to:
+        # type = template[0][:type] which would return :sites
+        # But I *think* in my illustrative example we would want type = :olts
+        
+        # I think above is a problem but sticking with scenario where first column *is* "site"...
+        # OLA for first column of first row:
+        # type = template[0][:type] = :sites
+        
+        # OLA for second column of first row:
+        # type = template[1][:type] = :buildings
         type = template[index][:type]
+        
         collection = template[index][:collection]
 
         # Do we have this node yet?
-        object = previous[type].select { |object| object[:value] == col }.first
+        object = previous[type].select { |object| object[:value] == col }.first # shouldn't be ".last" ?
+        # OLA: thinking you are looking for the last node created of the current type
+        # In that case should'nt we be looking for ".last" ?
 
         # If not, make it.
         if object.nil?
           object = { value: col, collection => [] }
+          # OLA for first column of first row: 
+          # object = { value: "site1", collection => [] }
+          # previous = graph = { sites: [] }
+          
+          # OLA for second column of first row: 
+          # object = { value: "building1", collection => [] }
+          # previous = { value: "site1", collection: [] } ...from first column of first row. See line 106 thru 108
           previous[type] << object
+          # OLA for first column of first row
+          # previous = { sites: [object] }
+          # graph = { sites: [object] }
+          
+          # OLA for second column of first row
+          # previous[:buildings] << object *** expect ERROR: undefined method `<<' for nil:NilClass. See line 94 ***
+          # graph = { sites: [object] }
         end
 
         # Set the current place in the nested structure for the next iteration
         previous = object
+        # OLA for first column of first row:
+        # previous = object = { value: "site1", collection: [] }
+        # graph = { sites: [object] }
+        
+        # OLA for second column of first row:
+        # previous = object = { value: "building1", collection: [] } ***I don't think it gets this far because line 101***
+        # graph = { sites: [object] }
       end
+      
+    # OLA for first row:
+    # graph = ?
+    # based on ine 107 and 111, looks to me like graph does not change from...
+    # graph = { sites: [object] }  ***I DON'T GET THIS***
     end
 
     graph
