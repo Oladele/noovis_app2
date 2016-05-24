94114885ba29286c7748b4fa0eaed6d56e62ec95
John Mosesman <johnmosesman@gmail.com>
2016-05-24 16:38:40 -0500

    Move to private

--- app/models/test_network_graph.rb
+++ app/models/test_network_graph.rb
@@ -19,61 +19,61 @@ class TestNetworkGraph < ActiveRecord::Base
 
   validates :graph, presence: true
 
-
   def nodes_and_edges
     iteration_helper = IterationHelper.new(1)
 
     graph["sites"].each do |site|
       collection = site[site.keys.last]
 
-      self.generate_for_collection(iteration_helper, collection, site.keys.last.to_s.singularize, 1, nil)
+      generate_for_collection(iteration_helper, collection, site.keys.last.to_s.singularize, 1, nil)
     end
 
     { nodes: iteration_helper.nodes, edges: iteration_helper.edges }
   end
 
-  def generate_for_collection(iteration_helper, collection, node_type, node_level, parent_id)
-    collection.each do |object|
-      node = {
-        id: iteration_helper.index,
-        label: "#{node_type.upcase}: #{object["value"]}",
-        cable_run_id: 22,       # TODO: do we need this?
-        network_graph_id: 55,   # TODO: do we need this?
-        node_level: node_level,
-        node_type: node_type,
-        node_value: object["value"],
-        parent_id: parent_id,
-        created_at: self.created_at,
-        updated_at: self.updated_at
-      }
-      iteration_helper.nodes << node
-
-      # If there is a parent, then we need to make an edge.
-      if parent_id.present?
-        edge = {
-          id: iteration_helper.index - 1,   # Offset so it starts on index 1, since it passes the first loop.
-          network_graph_id: 55, # TODO: do we need this?
-          to_node_id: node[:id],
-          from_node_id: parent_id,
-          edge_level: node_level - 1,       # Offset so it starts on index 1, since it passes the first loop.
+  private
+    def generate_for_collection(iteration_helper, collection, node_type, node_level, parent_id)
+      collection.each do |object|
+        node = {
+          id: iteration_helper.index,
+          label: "#{node_type.upcase}: #{object["value"]}",
+          cable_run_id: 22,       # TODO: do we need this?
+          network_graph_id: 55,   # TODO: do we need this?
+          node_level: node_level,
+          node_type: node_type,
+          node_value: object["value"],
+          parent_id: parent_id,
           created_at: self.created_at,
           updated_at: self.updated_at
         }
-        iteration_helper.edges << edge
-      end
+        iteration_helper.nodes << node
 
-      iteration_helper.increment
+        # If there is a parent, then we need to make an edge.
+        if parent_id.present?
+          edge = {
+            id: iteration_helper.index - 1,   # Offset so it starts on index 1, since it passes the first loop.
+            network_graph_id: 55, # TODO: do we need this?
+            to_node_id: node[:id],
+            from_node_id: parent_id,
+            edge_level: node_level - 1,       # Offset so it starts on index 1, since it passes the first loop.
+            created_at: self.created_at,
+            updated_at: self.updated_at
+          }
+          iteration_helper.edges << edge
+        end
 
-      nested_collection = object[object.keys.last]
+        iteration_helper.increment
 
-      if nested_collection.present?
-        # Ports have the same node level and parent_id
-        is_port_node = ["ont_ge_2_macs", "ont_ge_3_macs", "ont_ge_4_macs"].include?(object.keys.last)
-        next_node_level = is_port_node ? node_level : node_level + 1
-        previous_parent_id = is_port_node ? parent_id : node[:id]
+        nested_collection = object[object.keys.last]
 
-        self.generate_for_collection(iteration_helper, nested_collection, object.keys.last.to_s.singularize, next_node_level, previous_parent_id)
+        if nested_collection.present?
+          # Ports have the same node level and parent_id
+          is_port_node = ["ont_ge_2_macs", "ont_ge_3_macs", "ont_ge_4_macs"].include?(object.keys.last)
+          next_node_level = is_port_node ? node_level : node_level + 1
+          previous_parent_id = is_port_node ? parent_id : node[:id]
+
+          generate_for_collection(iteration_helper, nested_collection, object.keys.last.to_s.singularize, next_node_level, previous_parent_id)
+        end
       end
     end
-  end
 end
