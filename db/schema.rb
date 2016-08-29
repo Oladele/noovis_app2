b7171acf30e3363af6eff4d9c35296dd09a6b84d
John Mosesman <johnmosesman@gmail.com>
2016-08-29 14:05:18 -0500

    Change user tokens to string from json because of test error (https://github.com/lynndylanhurley/devise_token_auth/issues/365)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725162723) do
+ActiveRecord::Schema.define(version: 20160829190412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -169,7 +169,7 @@ ActiveRecord::Schema.define(version: 20160725162723) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.json     "tokens"
+    t.string   "tokens"
     t.integer  "role"
   end
 
